dea39413efea9956090c80bee38522877ec150d1
Sebasbl562 <gerardo.sebastian.bl@gmail.com>
2024-11-13 14:14:23 -0500

    Agrego los reosurce

---
+++ DentifyBackend/Odontology/Interfaces/REST/Resources/Inventory/UpdateInventoryResource.cs
@@ -0,0 +1,4 @@
+namespace DentifyBackend.Odontology.Interfaces.REST.Resources.Inventory;
+
+public record UpdateInventoryResource(string material_name, int quantity, float unit_price,
+    DateTime last_updated, int user_id);
