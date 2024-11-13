2278151f7b33574e4b16d3212e205dfc46111af4
Sebasbl562 <gerardo.sebastian.bl@gmail.com>
2024-11-13 14:11:57 -0500

    Agregacion de Entidad Inventory.cs y sus respectivos commands  y queries

---
+++ DentifyBackend/Odontology/Domain/Model/Aggregates/Inventory.cs
@@ -0,0 +1,29 @@
+using DentifyBackend.Odontology.Domain.Model.Commands.Inventory;
+
+namespace DentifyBackend.Dentify.Domain.Model.Aggregates;
+
+public class Inventory
+{
+    public int id { get; set; }
+    public string material_name { get; set; }
+    public int quantity { get; set; }
+    public float unit_price { get; set; }
+    public DateTime last_updated { get; set; }
+    public int user_id { get; set; }
+
+    public Inventory()
+    {
+        
+    }
+
+    public Inventory(CreateInventoryCommand command)
+    {
+        id = command.id;
+        material_name = command.material_name;
+        quantity = command.quantity;
+        unit_price = command.unit_price;
+        last_updated = command.last_updated;
+        user_id = command.user_id;
+    }
+    
+}
